8ca16e174a486080a49c073c1cf4faffbd40d3b4
Justin Duncan <b42161@mcast.org.mt>
2017-11-08 12:23:25 +0100

    08/11/17 - Paddle now has collision 	   Ball now has collision and bounce material 	   Lose, Win and Game can now be properly accessed as intended

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) // This is when both object collide to each other
+    {
+        //SceneManager.LoadScene("Lose");
+    }
+    private void OnTriggerEnter2D(Collider2D collision) // This replaces collision if property trigger is ticked on(Trigger removes the collision bounce)
+    {
+        SceneManager.LoadScene("Lose");
+    }
+}
---
+++ Assets/Scripts/PaddleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleManager : MonoBehaviour {
+
+    Vector3 paddlePosition;
+	// Use this for initialization
+	void Start () {
+        paddlePosition = transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            paddlePosition.x--;
+            transform.position = paddlePosition;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            paddlePosition.x++;
+            transform.position = paddlePosition;
+        }
+    }
+}
