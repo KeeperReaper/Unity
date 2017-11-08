82ae54ddd6d3fd214a7454b58ac05c36c15b8189
Justin Duncan <b42161@mcast.org.mt>
2017-11-08 10:50:41 +0100

    Imported brick breaker

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+    int min = 1, max = 1001, guess, life = 4;
+    public Text Guess;
+    // Use this for initialization
+    void Start () {
+        ChangeGuess();
+    }
+
+    public void gotoGameScene(String scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void changeMinMax(Boolean Higher) {
+        if (Higher) min = guess;
+        else max = guess;
+        ChangeGuess();
+    }
+
+    void ChangeGuess() {
+        life--;
+        guess = UnityEngine.Random.Range(min, max);
+        if (life != 0) Guess.text = "Current guess:\n " + guess;
+    }
+    // Update is called once per frame
+    void Update () {
+        if (life == 0) SceneManager.LoadScene("Win");
+    }
+}
---
+++ Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+    static MusicPlayerScript instance = null;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
